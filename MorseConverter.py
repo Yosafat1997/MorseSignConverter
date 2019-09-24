5dd67c4c4888f1fd77261d175ccd85bc5035b70e
yosafat <yosafat.vincent@bluebird.id>
2019-09-24 17:30:48 +0700

    implement 1st function

--- MorseConverter.py
+++ MorseConverter.py
@@ -1,6 +1,20 @@
 import dictionaries as de
 def Text2Morse(text):
     uppercaseText = text.upper()
+    listsText = uppercaseText.split(" ")
+    for g in listsText:
+        if g.isnumeric()==True:
+            temp = ""
+            for w in g:
+                temp+=de.dictionaryAtoM.get(chr(w))
+        else:
+            for k in g:
+
+    return " ".join(listsText)
+
+
+
+
 
 
 
